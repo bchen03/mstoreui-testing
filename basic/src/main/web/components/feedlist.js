a994e04a2dbdc8098de4bfb1f3d56e96d654c629
bchen03 <bchen03@yahoo.com>
2018-02-25 19:40:44 -0500

    Refactored Feed stateless component from FeedList

--- basic/src/main/web/components/feedlist.js
+++ basic/src/main/web/components/feedlist.js
@@ -4,6 +4,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import Feed from './feed';
+
 // FeedList
 class FeedList extends React.Component {
     constructor(props) {
@@ -11,41 +13,17 @@ class FeedList extends React.Component {
     }
 
     render() {
-        const cardStyle = {
-            width: "270px",
-            height: "370px",
-            marginTop: "20px",
-            marginRight: "20px"
-        };
-
-        const toObj = {
-            pathname: this.props.nexturl,
-            storeid: this.props.storeid
-        };
-
         const feedList = 
             this.props.feeds.length === 0 ?
             <div className="">No Data Sources exist</div> : 
             this.props.feeds.map(item => 
-                <div className="card" style={cardStyle} key={item.id}>
-                    <div className="card-body">
-                        <h3 className="card-title">
-                            <img className="img-fluid mr-3" src={item.img} width="50px" height="50px" alt={item.title} />
-                            <span style={{fontSize: "1.1rem"}}>{item.title}</span>
-                        </h3>
-                        <div className="card-text">
-                            {item.description}
-                        </div>
-                    </div>
-                    <div className="card-data">
-                        <Link 
-                            to={toObj} 
-                            role="button" 
-                            className="btn btn-sm btn-primary ml-3 mb-3">
-                            Subscribe
-                        </Link>
-                    </div>
-                </div>
+                <Feed key={item.id}
+                    storeid={this.props.storeid} 
+                    nexturl={this.props.nexturl} 
+                    id={item.id} 
+                    title={item.title} 
+                    description={item.description} 
+                    img={item.img} />
             );
 
         return (
@@ -65,3 +43,4 @@ FeedList.propTypes = {
 }
 
 export default FeedList;
+
