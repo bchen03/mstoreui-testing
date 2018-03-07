345a9e58aede4251ab046cba0408da0f9441533d
bchen03 <bchen03@yahoo.com>
2018-03-06 20:50:44 -0500

    Added Render Prop for StoreListContainer. Changed StoreListFacc to use PureComponent. Added spreadTest function to look at spread operator.

--- basic/src/main/web/containers/storelistcontainer.js
+++ basic/src/main/web/containers/storelistcontainer.js
@@ -1,12 +1,29 @@
 'use strict';
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import StoreList from '../components/storelist';
 
 import {mapStoreListState} from '../selectors/storelistselector';
 
+// getStores
+const getStores = (successFunc, failureFunc) => {
+    axios.get('http://localhost:8090/v1/stores', {
+        // params: {
+        //   ID: 12345
+        // }
+    })
+    .then(response => {
+        !!successFunc && successFunc(response);
+    }) 
+    .catch(err => {
+        !!successFunc && failureFunc(err);
+    });
+};
+
+// HOC
 function withStoreList(WrappedComponent) {
     return class extends React.Component {
         constructor(props) {
@@ -32,20 +49,14 @@ function withStoreList(WrappedComponent) {
                 error: ""
             });
 
-            axios.get('http://localhost:8090/v1/stores', {
-                // params: {
-                //   ID: 12345
-                // }
-            })
-            .then(response => {
+            getStores(response => {
                 console.log("withStoreList.getData success, data: ", response.data.data);
                 this.setState({
                     stores: mapStoreListState(response.data.data),
                     isLoading: false,
                     error: ""
                 });
-            }) 
-            .catch(err => {
+            }, err => {
                 console.log("withStoreList.getData failed: ", err);
                 this.setState({
                     stores: [],
@@ -53,6 +64,29 @@ function withStoreList(WrappedComponent) {
                     error: err
                 });
             });
+
+
+            // axios.get('http://localhost:8090/v1/stores', {
+            //     // params: {
+            //     //   ID: 12345
+            //     // }
+            // })
+            // .then(response => {
+            //     console.log("withStoreList.getData success, data: ", response.data.data);
+            //     this.setState({
+            //         stores: mapStoreListState(response.data.data),
+            //         isLoading: false,
+            //         error: ""
+            //     });
+            // }) 
+            // .catch(err => {
+            //     console.log("withStoreList.getData failed: ", err);
+            //     this.setState({
+            //         stores: [],
+            //         isLoading: false,
+            //         error: err
+            //     });
+            // });
         }
 
         getData2() {
@@ -128,6 +162,100 @@ function withStoreList(WrappedComponent) {
     }
 } 
 
-const StoreListContainer = withStoreList(StoreList);
+//const StoreListContainer = withStoreList(StoreList);
+
+// Function as Child Component (FACC, also known as RenderProps)
+// Also using PureComponent which implements shouldComponentUpdate() 
+// and does a shallow comparison on props/state
+class StoreListFacc extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            stores: [],
+            isLoading: false,
+            error: ""
+        }
+
+        this.spreadTests = this.spreadTests.bind(this);
+        this.getData = this.getData.bind(this);
+
+        this.spreadTests();
+    }
+
+    spreadTests() {
+        console.warn("Spread Tests");
+        let a = [1,2,3,4];
+        let b = [...a, 5,6];
+        console.warn("==> b: ", b);
+
+        let c = { a: "A", b: "B", c: "C"};
+        let d = { ...c};
+        console.warn("==> d: ", d);
+
+        let { a: aa, ...x } = d;
+        console.warn("==> a: ", aa, ", ...x: ", x);
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        this.setState({
+            stores: [],
+            isLoading: true,
+            error: ""
+        });
+
+        getStores(response => {
+            console.log("withStoreList.getData success, data: ", response.data.data);
+            this.setState({
+                stores: mapStoreListState(response.data.data),
+                isLoading: false,
+                error: ""
+            });
+        }, err => {
+            console.log("withStoreList.getData failed: ", err);
+            this.setState({
+                stores: [],
+                isLoading: false,
+                error: err
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                { this.props.children(this.state.stores, this.state.isLoading, this.state.error) }
+            </div>
+        );
+    }
+}
+
+StoreListFacc.propTypes = {
+    children: PropTypes.func.isRequired
+}
+
+const StoreListContainer = () => {
+    return (
+        <StoreListFacc>
+            {
+                (stores, isLoading, error) => {
+                    console.log("StoreListContainer.children stores: ", stores, 
+                                ", isLoading: ", isLoading, ", error: ", error.toString());
+                    return isLoading ? <div>Loading data, please wait...</div> :
+                        error !== "" ? <div>Error: {error.toString()}</div> : 
+                        <StoreList stores={stores} {...this.props} />;
+                }
+            }
+        </StoreListFacc>
+    );
+}
 
 export default StoreListContainer;
+
+
+
+
